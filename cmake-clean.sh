2db07b8ab402b85c70c1d93843d1837a990d10fa
Donovan Hutcheon <donovan.hutcheon@concurrent.systems>
2019-10-02 17:39:20 +0200

    Step 3 complete: make install and tests

--- cmake-clean.sh
+++ cmake-clean.sh
@@ -2,3 +2,6 @@
 
 rm -rf {CMakeCache.txt,CMakeFiles,cmake.check_cache,Makefile,cmake_install.cmake,Tutorial,TutorialConfig.h}
 rm -rf MathFunctions/{CMakeFiles,Makefile,cmake_install.cmake,libMathFunctions.a}
+#Step3:
+rm -rf {CTestTestfile.cmake,DartConfiguration.tcl,Testing,install_manifest.txt}
+
