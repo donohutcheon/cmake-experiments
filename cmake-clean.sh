4299c26fc6a617faba94ccaa46a2eb0ee33cb92a
Donovan Hutcheon <donovan.hutcheon@concurrent.systems>
2019-10-02 16:51:10 +0200

    Adding bash script to clean out cmake crap.

---
+++ cmake-clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf {CMakeCache.txt,CMakeFiles,cmake.check_cache,Makefile,cmake_install.cmake,Tutorial,TutorialConfig.h}
+rm -rf MathFunctions/{CMakeFiles,Makefile,cmake_install.cmake,libMathFunctions.a}
